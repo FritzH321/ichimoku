a710013c0ae1e6ad0c9c778b87a13d05cd081546
Fritz Hoste <hoste.fritz@gmail.com>
2018-01-05 12:28:26 +0100

    add laggingspan

--- src/index.js
+++ src/index.js
@@ -1,92 +1,106 @@
 class Ichimoku {
-	constructor(input) {
-		let _this = this
+    constructor(input) {
+        let _this = this;
 
-		this.defaults = {
-			conversionPeriod : 9,
-			basePeriod       : 26,
-			spanPeriod       : 52,
-			displacement     : 26
-		}
+        this.defaults = {
+            conversionPeriod : 9,
+            basePeriod       : 26,
+            spanPeriod       : 52,
+            displacement     : 26
+        };
 
-		// Overwrite param defaults
-		this.params = Object.assign({}, this.defaults, input)
+        // Overwrite param defaults
+        this.params = Object.assign({}, this.defaults, input);
 
-		this.generator = (function* () {
-			let result
-			let tick
+        this.generator = (function* () {
+            let result;
+            let tick;
 
-			let period = Math.max(_this.params.conversionPeriod, _this.params.basePeriod, _this.params.spanPeriod, _this.params.displacement)
-			let periodCounter = 0
-			let spanCounter = 0
-			let highs = []
-			let lows = []
-			let spanAs = []
-			let spanBs = []
+            let period = Math.max(_this.params.conversionPeriod, _this.params.basePeriod, _this.params.spanPeriod, _this.params.displacement);
+            let periodCounter = 0;
+            let spanCounter = 0;
+            let lagCounter = 0;
+            let highs = [];
+            let lows = [];
+            let closes = [];
+            let spanAs = [];
+            let spanBs = [];
 
-			let conversionPeriodLow, conversionPeriodHigh
-			let basePeriodLow, basePeriodHigh
-			let spanbPeriodLow, spanbPeriodHigh
+            let conversionPeriodLow, conversionPeriodHigh;
+            let basePeriodLow, basePeriodHigh;
+            let spanbPeriodLow, spanbPeriodHigh;
 
-			tick = yield
-			
-			while (true) {
-				// Keep a list of lows/highs for the max period
-				highs.push(tick.high)
-				lows.push(tick.low)
+            tick = yield;
 
-				if(periodCounter < period) {
-					periodCounter++
-				} else {
-					highs.shift()
-					lows.shift()
+            while (true) {
+                // Keep a list of lows/highs for the max period
+                highs.push(tick.high);
+                lows.push(tick.low);
+                // Keep a list of closes for the lagging span
+                closes.push(tick.close);
 
-					// Tenkan-sen (ConversionLine): (9-period high + 9-period low)/2))
-					conversionPeriodLow = lows.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.min(a,b) )
-					conversionPeriodHigh = highs.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.max(a,b) )
-					let conversionLine = (conversionPeriodHigh + conversionPeriodLow) /2
+                if(periodCounter < period) {
+                    periodCounter++
+                } else {
+                    highs.shift();
+                    lows.shift();
 
-					// Kijun-sen (Base Line): (26-period high + 26-period low)/2))
-					basePeriodLow = lows.slice(-_this.params.basePeriod).reduce( (a,b) => Math.min(a,b) )
-					basePeriodHigh = highs.slice(-_this.params.basePeriod).reduce( (a,b) => Math.max(a,b) )
-					let baseLine = (basePeriodHigh + basePeriodLow) /2
+                    // Tenkan-sen (ConversionLine): (9-period high + 9-period low)/2))
+                    conversionPeriodLow = lows.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.min(a,b) );
+                    conversionPeriodHigh = highs.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.max(a,b) );
+                    let conversionLine = (conversionPeriodHigh + conversionPeriodLow) /2;
 
-					// Senkou Span A (Leading Span A): (Conversion Line + Base Line)/2))
-					let spanA = 0
-					spanAs.push((conversionLine + baseLine) /2)
+                    // Kijun-sen (Base Line): (26-period high + 26-period low)/2))
+                    basePeriodLow = lows.slice(-_this.params.basePeriod).reduce( (a,b) => Math.min(a,b) );
+                    basePeriodHigh = highs.slice(-_this.params.basePeriod).reduce( (a,b) => Math.max(a,b) );
+                    let baseLine = (basePeriodHigh + basePeriodLow) /2;
 
-					// Senkou Span B (Leading Span B): (52-period high + 52-period low)/2))
-					let spanB = 0
-					spanbPeriodLow = lows.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.min(a,b) )
-					spanbPeriodHigh = highs.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.max(a,b) )
-					spanBs.push((spanbPeriodHigh + spanbPeriodLow) /2)
+                    // Senkou Span A (Leading Span A): (Conversion Line + Base Line)/2))
+                    let spanA = 0;
+                    spanAs.push((conversionLine + baseLine) /2);
 
-					// Senkou Span A / Senkou Span B offset by 26 periods
-					if(spanCounter < _this.params.displacement) {
-						spanCounter++
-					} else {
-						spanA = spanAs.shift()
-						spanB = spanBs.shift()
-					}
+                    // Senkou Span B (Leading Span B): (52-period high + 52-period low)/2))
+                    let spanB = 0;
+                    spanbPeriodLow = lows.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.min(a,b) );
+                    spanbPeriodHigh = highs.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.max(a,b) );
+                    spanBs.push((spanbPeriodHigh + spanbPeriodLow) /2);
 
-					result = {
-						conversion : parseFloat(conversionLine.toFixed(5)),
-						base       : parseFloat(baseLine.toFixed(5)),
-						spanA      : parseFloat(spanA.toFixed(5)),
-						spanB      : parseFloat(spanB.toFixed(5))
-					}
+                    // Senkou Span A / Senkou Span B offset by 26 periods
+                    if(spanCounter < _this.params.displacement) {
+                        spanCounter++
+                    } else {
+                        spanA = spanAs.shift();
+                        spanB = spanBs.shift()
+                    }
 
-				}
+                    // Chikou Span (Lagging Span): Close plotted 26 days in the past
+                    let close = 0;
+                    if(lagCounter > _this.params.displacement) {
+                        lagCounter++
+                    } else {
+                        close = parseFloat(closes.shift())
+                    }
 
-				tick = yield result
-			}
-		})()
-		
-	}
-	nextValue(price) {
-		return this.generator.next(price).value;
-	}
+
+                    result = {
+                        conversion : parseFloat(conversionLine.toFixed(8)),
+                        base       : parseFloat(baseLine.toFixed(8)),
+                        spanA      : parseFloat(spanA.toFixed(8)),
+                        spanB      : parseFloat(spanB.toFixed(8)),
+                        lagging	   : parseFloat(close.toFixed(8))
+                    }
+
+                }
+
+                tick = yield result
+            }
+        })()
+
+    }
+    nextValue(price) {
+        return this.generator.next(price).value
+    }
 
 }
 
-module.exports = Ichimoku
+module.exports = Ichimoku;
--- test/test.js
+++ test/test.js
@@ -4,105 +4,117 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 
 describe('Ichimoku', () => {
-	const ichimoku = new Ichimoku({conversionPeriod: 9, basePeriod: 26, spanPeriod: 52, values: []})
+    const ichimoku = new Ichimoku({conversionPeriod: 9, basePeriod: 26, spanPeriod: 52, values: []})
 
-	var ichimokuValue = {}
-	var ichimokuValues = []
+    var ichimokuValue = {}
+    var ichimokuValues = []
 
-	beforeEach(() => {
-		for( let candle of candles ) {
-			ichimokuValues.push(
-				ichimoku.nextValue({
-					high  : candle.mid.h,
-					low   : candle.mid.l,
-					close : candle.mid.c
-				})
-			)
-		}
+    beforeEach(() => {
+        for (let candle of candles) {
+            ichimokuValues.push(
+                ichimoku.nextValue({
+                    high: candle.mid.h,
+                    low: candle.mid.l,
+                    close: candle.mid.c
+                })
+            )
+        }
 
-		ichimokuValue = ichimokuValues.slice(-1).pop()
-		ichimokuValues = ichimokuValues.slice(-10)
+        ichimokuValue = ichimokuValues.slice(-1).pop()
+        ichimokuValues = ichimokuValues.slice(-10)
 
-  })
+    })
 
-	it('should return a conversion line', () => {
-		expect(ichimokuValue).to.contain.key('conversion')
-	})
+    it('should return a conversion line', () => {
+        expect(ichimokuValue).to.contain.key('conversion')
+    })
 
-	it('should return a base line', () => {
-		expect(ichimokuValue).to.contain.key('base')
-	})
+    it('should return a base line', () => {
+        expect(ichimokuValue).to.contain.key('base')
+    })
 
-	it('should return a Span A line', () => {
-		expect(ichimokuValue).to.contain.key('spanA')
-	})
+    it('should return a Span A line', () => {
+        expect(ichimokuValue).to.contain.key('spanA')
+    })
 
-	it('should return a Span B line', () => {
-		expect(ichimokuValue).to.contain.key('spanB')
-	})
+    it('should return a Span B line', () => {
+        expect(ichimokuValue).to.contain.key('spanB')
+    })
 
-	it('should calculate the correct line values', () => {
-		expect(ichimokuValues).to.deep.equal([
-				{
-						conversion: 1.3393,
-						base: 1.33666,
-						spanA: 1.3376,
-						spanB: 1.33744
-				},
-				{
-						conversion: 1.33921,
-						base: 1.33666,
-						spanA: 1.33748,
-						spanB: 1.33744
-				},
-				{
-						conversion: 1.33899,
-						base: 1.33666,
-						spanA: 1.33752,
-						spanB: 1.33744
-				},
-				{
-						conversion: 1.33899,
-						base: 1.33666,
-						spanA: 1.33759,
-						spanB: 1.33736
-				},
-				{
-						conversion: 1.339,
-						base: 1.33668,
-						spanA: 1.33783,
-						spanB: 1.33736
-				},
-				{
-						conversion: 1.33956,
-						base: 1.33723,
-						spanA: 1.33791,
-						spanB: 1.33736
-				},
-				{
-						conversion: 1.33956,
-						base: 1.33723,
-						spanA: 1.33791,
-						spanB: 1.33736
-				},
-				{
-						conversion: 1.33956,
-						base: 1.33723,
-						spanA: 1.33771,
-						spanB: 1.33736
-				},
-				{
-						conversion: 1.33982,
-						base: 1.3375,
-						spanA: 1.33771,
-						spanB: 1.33716
-				},
-				{
-						conversion: 1.33982,
-						base: 1.3375,
-						spanA: 1.33811,
-						spanB: 1.33709
-				}
-			])
-	})
-})
+    it('should return a Lagging span', () => {
+        expect(ichimokuValue).to.contain.key('lagging')
+    })
+
+    it('should calculate the correct line values', () => {
+        expect(ichimokuValues).to.deep.equal([{
+            conversion: 1.3393,
+            base: 1.336665,
+            spanA: 1.3375975,
+            spanB: 1.337445,
+            lagging: 1.3333
+        },
+            {
+                conversion: 1.33921,
+                base: 1.336665,
+                spanA: 1.33748,
+                spanB: 1.337445,
+                lagging: 1.33332
+            },
+            {
+                conversion: 1.33899,
+                base: 1.336665,
+                spanA: 1.337525,
+                spanB: 1.337445,
+                lagging: 1.33352
+            },
+            {
+                conversion: 1.33899,
+                base: 1.336665,
+                spanA: 1.3375875,
+                spanB: 1.33736,
+                lagging: 1.33326
+            },
+            {
+                conversion: 1.339,
+                base: 1.336675,
+                spanA: 1.3378325,
+                spanB: 1.33736,
+                lagging: 1.3341
+            },
+            {
+                conversion: 1.33956,
+                base: 1.337235,
+                spanA: 1.33791,
+                spanB: 1.33736,
+                lagging: 1.33465
+            },
+            {
+                conversion: 1.33956,
+                base: 1.337235,
+                spanA: 1.33791,
+                spanB: 1.33736,
+                lagging: 1.33501
+            },
+            {
+                conversion: 1.33956,
+                base: 1.337235,
+                spanA: 1.337715,
+                spanB: 1.33736,
+                lagging: 1.33518
+            },
+            {
+                conversion: 1.33982,
+                base: 1.337495,
+                spanA: 1.337715,
+                spanB: 1.33716,
+                lagging: 1.33504
+            },
+            {
+                conversion: 1.33982,
+                base: 1.337495,
+                spanA: 1.33811,
+                spanB: 1.33709,
+                lagging: 1.33506
+            }])
+    })
+});
